24bbab54b31383b67361fdad928ab5093e6ed9d2
NataliaGatarek <nova.nathalia@gmail.com>
2021-06-19 12:04:38 +0200

    1st commit

---
+++ car.js
@@ -0,0 +1,129 @@
+// We define a class Car
+class Car {
+  // The constructor initializes the properties.
+  // It gets called when we do: new Car(somBrand, someModel)
+  // We initialize the speed to 0
+  constructor(brand, model) {
+    this.brand = brand;
+    this.model = model;
+      this.speed = 0;
+       this.motion ="The car is not moving "
+  }
+
+  // Car class methods:
+  // (notice that now we don't say "function" and that we
+  //  don't put a comma after closing brace of the function)
+    
+    check_motion() {
+        (this.speed > 0) ? this.motion = "Moving" : this.motion = "Not Moving"
+    }
+
+  accelerate(amount) {
+      this.speed += amount;
+       this.check_motion()
+  }
+
+  brake(amount) {
+    if (amount <= this.speed) {
+      this.speed -= amount;
+    } else {
+      // a car cannot go to negative speed
+      this.speed = 0;
+      }
+          this.check_motion();
+  }
+
+  stopTheCar() {
+      this.speed = 0;
+       this.check_motion()
+    }
+    
+
+  status() {
+    return (
+      this.brand + " " + this.model + " running at " + this.speed + " km/h"+ this.motion
+    );
+  }
+}
+
+// Now we use create a Car object using the class
+
+const car = new Car("Ford", "Mondeo");
+
+console.log(car.status());
+
+// we make the car accelerate and check it's status
+car.accelerate(50);
+console.log(car.status());
+
+car.accelerate(100);
+console.log(car.status());
+
+// now the car will brake and we check the status
+car.brake(25);
+console.log(car.status());
+
+car.brake(25);
+console.log(car.status());
+
+car.brake(10);
+console.log(car.status());
+
+// at the end of the trip we make the car stop
+car.stopTheCar();
+console.log(car.status());
+
+// Thanks to class definition we can now create other cars easily
+const car2 = new Car("BMW", "Mini Cooper");
+car2.accelerate(200);
+console.log(car2.status());
+
+// Let's now create my favorite car and display some information about it in the HTML
+window.onload = () => {
+  const myFavCar = new Car("Nissan", "Micra");
+  displayMyFavCar(myFavCar);
+  displaySpeed(myFavCar);
+  addEvents(myFavCar);
+};
+const displayMyFavCar = (myFavCar) => {
+  const carBrand = document.getElementById("car-brand");
+  carBrand.innerHTML = myFavCar.brand;
+  const carModel = document.getElementById("car-model");
+  carModel.innerHTML = myFavCar.model;
+};
+
+const displaySpeed = (myFavCar) => {
+  const carSpeed = document.getElementById("car-speed");
+  carSpeed.innerHTML = myFavCar.speed;
+};
+const displayIsRunning = (myFavCar) => {
+    const carIsRunning = document.getElementById("car-motion");
+    carIsRunning.innerHTML=myFavCar.motion
+}
+const addEvents = (myFavCar) => {
+  const accelerateButton = document.getElementById("accelerate");
+  accelerateButton.addEventListener("click", () => accelerate(myFavCar));
+  const brakeButton = document.getElementById("brake");
+  brakeButton.addEventListener("click", () => brake(myFavCar));
+  const stopButton = document.getElementById("stop");
+  stopButton.addEventListener("click", () => stop(myFavCar));
+};
+const accelerate = (myFavCar) => {
+  myFavCar.accelerate(20);
+  displaySpeed(myFavCar);
+};
+
+const brake = (myFavCar) => {
+  myFavCar.brake(20);
+  displaySpeed(myFavCar);
+};
+
+const stop = (myFavCar) => {
+  myFavCar.stopTheCar();
+  displaySpeed(myFavCar);
+};
+const motion = (myFavCar) => {
+    myFavCar.motion();
+    displayIsRunning(myFavCar);
+}
+// Create a method inside the class definition that checks wether the Car is running or not and display it in the HTML
---
+++ tv.js
@@ -0,0 +1,77 @@
+class Tv {
+    constructor(brand, channel, volume) {
+        this.brand = brand;
+        this.volume = volume;
+        this.channel = channel;
+        this.mirror_volume = this.volume;
+        this.mirror_channel = this.channel;
+    }
+     increase_volume(amount) {
+        this.volume += amount;
+        (this.volume > 100) ? this.volume = 100 : this.volume = this.volume;
+    }
+    
+    decrease_volume(amount) {
+        this.volume -= amount;
+        (this.volume < 0) ? this.volume = 0 : this.volume = this.volume;
+    }
+      set_channel(number) {
+        (number > 50 || number <=0) ? this.channel = this.channel : this.channel = number;
+    }
+    reset(){
+        this.volume = this.mirror_volume;
+        this.channel = this.mirror_channel;
+    }
+     status() {
+        return this.brand + " " + this.model + " " + " at channel " + this.channel + " and volume " +  this.volume
+    }
+}
+
+window.onload = () => {
+  const tv = new Tv("LG", "4", 40);
+  displaytv(tv);
+  addEvents(tv);
+};
+const displaytv = (tv) => {
+  const tvBrand = document.getElementById("tv-brand");
+  tvBrand.innerHTML = tv.brand;
+  const tvVolume = document.getElementById("tv-volume");
+    tvVolume.innerHTML = tv.volume;
+    const tvChannel = document.getElementById("tv-channel");
+  tvChannel.innerHTML = tv.channel;
+};
+const addEvents = (tv) => {
+  const increaseButton = document.getElementById("increase-volume");
+  increaseButton.addEventListener("click", () => increase(tv));
+  const decreaseButton = document.getElementById("decrease-volume");
+ decreaseButton .addEventListener("click", () => decrease(tv));
+  const channelButton = document.getElementById("change-channel");
+    channelButton.addEventListener("click", () => channel(tv));
+    const channelOne = document.getElementById("channel");
+    channelOne.addEventListener("click", () => change(tv));
+     const resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", () => reset(tv));
+};
+const increase = (tv) => {
+  tv.increase_volume(1);
+  displaytv(tv);
+};
+const decrease = (tv) => {
+  tv.decrease_volume(1);
+  displaytv(tv);
+};
+const channel = (tv) => {
+    tv.set_channel(change);
+    displaytv(tv)
+}
+const reset = (tv) => {
+    tv.reset(tv);
+    displaytv(tv)
+}
+
+//1) Create a TV class with properties like brand, channel and volume.
+//    Specify brand in a constructor parameter. Channel should be 1 by default. Volume should be 50 by default.
+//2) Add methods to increase and decrease volume. Volume can't never be below 0 or above 100.
+//3) Add a method to set the channel. Let's say the TV has only 50 channels so if you try to set channel 60 the TV will stay at the current channel.
+//4) Add a method to reset TV so it goes back to channel 1 and volume 50. (Hint: consider using it from the constructor).
+//5) It's useful to write a status, that returns info about the TV status like: "Panasonic at channel 8, volume 75".
